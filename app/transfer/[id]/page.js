aa3fec479f5b03dc650b6a61677430749c9fa805
Yerkanat <sayranbekov.0000@gmail.com>
2023-10-18 01:47:18 +0600

    creatio form

--- app/transfer/[id]/page.js
+++ app/transfer/[id]/page.js
@@ -40,7 +40,7 @@ export default function Home({ params }) {
     args: [address, toAddress, NFT_ID?.data],
   })
 
-  const { isSuccess:isBurned, write:burn } = useContractWrite({
+  const { isSuccess: isBurned, write: burn } = useContractWrite({
     address: '0x7Cc4390fa2557AA795467059dA753d76DeE71C35', // Remix
     abi: NFT.abi,
     functionName: 'burn',
@@ -98,7 +98,7 @@ export default function Home({ params }) {
 
   return (
     <main className="flex min-h-[80vh] flex-col items-center justify-between pt-24">
-      <div className="flex relative flex-col h-48 w-full items-center gap-4 justify-start lg:justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div className="flex relative flex-col h-48 items-center gap-4 justify-start lg:justify-center lg:static lg:h-auto w-auto bg-none">
 
         <div key={1} class="w-full max-w-sm z-20 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-600 dark:border-gray-700">
           <img class="p-8 rounded-xl" src={nftData.image} alt='IPFS Image' />
@@ -139,29 +139,27 @@ export default function Home({ params }) {
       {show &&
 
 
-        <div class="absolute w-full max-w-md max-h-full z-40">
-          <div class=" absolute bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal" onClick={() => {
+        <div class=" absolute bg-white rounded-lg shadow dark:bg-gray-700 z-40">
+          <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal" onClick={() => {
             setShow(false)
           }}>
-              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-              </svg>
-              <span class="sr-only">Close modal</span>
-            </button>
-            <div class="p-6 text-center">
-              <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-              </svg>
-              <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to burn this NFT?</h3>
-              <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2" 
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+          <div class="p-6 text-center">
+            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to burn this NFT?</h3>
+            <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
               onClick={burn}>
-                Yes, I'm sure
-              </button>
-              <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" onClick={() => {
-            setShow(false)
-          }}>No, cancel</button>
-            </div>
+              Yes, I'm sure
+            </button>
+            <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" onClick={() => {
+              setShow(false)
+            }}>No, cancel</button>
           </div>
         </div>
 
