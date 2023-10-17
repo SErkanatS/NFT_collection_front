587bbb01bf649f3b4e6f11a6a5de20528d1c3237
Yerkanat <sayranbekov.0000@gmail.com>
2023-10-18 01:22:23 +0600

    creatio form

--- app/create/page.js
+++ app/create/page.js
@@ -3,7 +3,7 @@ import { useContractWrite, useAccount, useContractEvent } from 'wagmi'
 import NFT from '../../artifacts/contracts/CryptoAqsha.sol/CryptoAqsha.json'
 import { toast } from 'react-toastify';
 import { NFTStorage, File } from 'nft.storage'
-import { useState  } from 'react'
+import { useState } from 'react'
 import { ImSpinner } from "react-icons/im";
 import { useRouter } from 'next/navigation';
 
@@ -51,30 +51,30 @@ export default function Home() {
   }
   return (
     <main className="flex min-h-[80vh] flex-col items-center justify-between pt-24">
-      <div className="flex flex-col h-48 w-full items-center gap-4 justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        <input type="text" id="default-input" placeholder='Give your NFT name' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          onChange={(e) => { setName(e.target.value) }}
-        />
-
-        <textarea id="message" rows="4" className="block p-2.5 w-full text-sm resize-none text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your description here..."
-          onChange={(e) => { setDescription(e.target.value) }}
-        ></textarea>
-
-        <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"
-          onChange={(e) => { if (e.target.files) { setFile(e.target.files[0]) } }} />
-        <button type="submit" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" onClick={uploadNFT}>
-          {
-            isLoading ? (
-              <ImSpinner className=' animate-spin' />
-            )
-              :
-              (
-                <p>Upload</p>
+      <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form class="space-y-6">
+          <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+          <input type="text" id="default-input" placeholder='Give your NFT name' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            onChange={(e) => { setName(e.target.value) }}
+          />
+          <textarea id="message" rows="4" className="block p-2.5 w-full text-sm resize-none text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your description here..."
+            onChange={(e) => { setDescription(e.target.value) }}
+          ></textarea>
+          <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"
+            onChange={(e) => { if (e.target.files) { setFile(e.target.files[0]) } }} />
+          <button type="submit" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" onClick={uploadNFT}>
+            {
+              isLoading ? (
+                <ImSpinner className=' animate-spin' />
               )
-          }
-        </button>
+                :
+                (
+                  <p>Upload</p>
+                )
+            }
+          </button>
+        </form>
       </div>
-
     </main>
   )
 }
